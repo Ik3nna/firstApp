601a787544aa7c654ee3d1abb6eaa4c4d2f471d7
ik3nna <iamiykendukwu@gmail.com>
2023-09-01 20:21:46 +0100

    feat: Drawers

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator  } from "@react-navigation/bottom-tabs";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from './src/screens/home';
 import About from './src/screens/about';
 
@@ -8,45 +7,28 @@ import About from './src/screens/about';
 import { FontAwesome } from '@expo/vector-icons'; 
 import { SafeAreaView } from 'react-native';
 
-// const Tab = createBottomTabNavigator();
-// const Tab = createMaterialBottomTabNavigator();
-const Tab = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route})=>({
-            tabBarIcon: ({ focused, size, color })=>{
-              let iconName;
-              if (route.name === "Home") {
-                iconName="home";
-                size=focused ? 25 : 20;
-                // color=focused && "blue"
-              } else if (route.name === "About") {
-                iconName="info-circle";
-                size=focused ? 25 : 20;
-                // color=focused && "blue"
-              }
-              return (
-                <FontAwesome name={iconName} size={size} color={color} />
-              )
-            }
-          })}
-          // activeColor='#f0edf6'
-          // inactiveColor='#3e2465'
-          // barStyle={{ backgroundColor: "#694fad" }}
-          tabBarOptions={{
-            activeTintColor: 'blue',
-            inactiveTintColor: "black",
-            labelStyle: { fontSize: 20 },
+        <Drawer.Navigator 
+          screenOptions={{
+            drawerPosition: "right",
+            drawerType: "front",
+            overlayColor: "#00000099",
+            drawerHideStatusBarOnOpen: true,
+            swipeEdgeWidth: 500,
+            drawerStyle: { backgroundColor: "yellow" },
+            gestureHandlerProps: false,
+            headerTitleAlign: "center"
           }}
         >
-          <Tab.Screen name="Home" component={Home} /> 
-          <Tab.Screen name="About" component={About} />
-        </Tab.Navigator>
+          <Drawer.Screen name="Home" component={Home} options={{ drawerIcon: ()=>(<FontAwesome name="home" size={24} />) }} /> 
+          <Drawer.Screen name="About" component={About} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
