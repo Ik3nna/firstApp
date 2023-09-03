a60a1eb185097b1a182fafabd3a78f3219f5250d
ik3nna <iamiykendukwu@gmail.com>
2023-09-03 14:53:10 +0100

    feat: Local notification

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,22 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Text, Pressable, StyleSheet, SafeAreaView, Alert, TextInput } from "react-native";
+import { useCallback, useEffect, useState, useRef } from 'react';
+import { Text, Pressable, StyleSheet, SafeAreaView, Alert, TextInput, View, TouchableOpacity } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 // styles
 import globalStyle from '../components/globalStyle';
+import { FlatList } from 'react-native-gesture-handler';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const Home = ({ navigation, route }) => {
   const [fontsLoaded] = useFonts({
@@ -19,105 +29,136 @@ const Home = ({ navigation, route }) => {
     }
   }, [fontsLoaded]);
 
-  const [name, setName] = useState("");
+  const DATA = [
+    {
+      title: 'United States',
+      subtitle: "New York"
+    },
+    {
+      title: 'Australia',
+      subtitle: "Sydney"
+    },
+    {
+      title: 'Germany',
+      subtitle: "Berlin"
+    },
+    {
+      title: 'France',
+      subtitle: "Paris"
+    },
+  ];
 
-  useEffect(()=>{
-    getData();
-  },[]);
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
-  const getData = ()=> {
-    try {
-      AsyncStorage.getItem("Username").then(value => {
-        if (value != null) {
-          setName(value);
-        }
-      })
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
-  const updateData = async ()=> {
-    if (name.length === 0) {
-        Alert.alert("Warning", "Please enter your name!!")
-    }
-    else {
-        try {
-            await AsyncStorage.setItem("Username", name);
-            Alert.alert("Success", "Your name has been updated")
-        } catch (error) {
-            console.log(error)
-        }
-    }
-  }
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
 
-  const removeData = async ()=> {
-    try {
-      await AsyncStorage.removeItem("Username");
-      navigation.navigate("Login")
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <SafeAreaView style={[globalStyle.body, ]} onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
-      <Text style={[globalStyle.text, styles.text]}>Welcome {name}</Text>
+      <Text style={[globalStyle.text, styles.text]}>Welcome Ikenna</Text>
 
-      <TextInput 
-        style={styles.input}
-        value={name}
-        placeholder="Update your name"
-        onChangeText={(value)=>setName(value)}
+      <FlatList 
+        keyExtractor={(item, index)=> index.toString()}
+        data={DATA}
+        renderItem={({item})=>(
+          <TouchableOpacity onPress={async () => { await schedulePushNotification(item) }}>
+            <View style={styles.view}>
+              <Text style={styles.mainText}>{item.title}</Text>
+
+              <Text style={styles.text}>{item.subtitle}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
       />
-
-      <Pressable
-        style={({pressed})=> 
-        [
-          { backgroundColor: pressed ? "#ddd" : "#0f0" },
-          globalStyle.button
-        ]}
-        onPress={updateData}
-      >
-        <Text>Update your name</Text>
-      </Pressable>
-
-      <Pressable
-        style={({pressed})=> 
-        [
-          { backgroundColor: pressed ? "#ddd" : "#0f0" },
-          globalStyle.button, { marginTop: 10 }
-        ]}
-        onPress={removeData}
-      >
-        <Text>Delete your name</Text>
-      </Pressable>
     </SafeAreaView>
   )
 }
 
 export default Home;
 
-const styles = StyleSheet.create({
-    text: {
-      fontFamily: "Roboto-Thin"
+async function schedulePushNotification (item) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: `${item.title} ${"\n"}Please share this information to me`,
+      body: item.subtitle,
+      data: { customData: 'Please share this information to me' },
     },
-    input: {
-      width: 300,
-      height: 50,
-      borderWidth: 1,
-      borderColor: "#555",
-      borderRadius: 10,
-      textAlign: "center",
-      fontSize: 20,
-      marginTop: 130,
-      marginBottom: 20,
-      backgroundColor: "#ffffff"
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Roboto-Thin"
+  },
+  view: {
+    width: 300,
+    margin: 7,
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderWidth: 2,
+    borderColor: "#cccccc",
+    padding: 20
+  },
+  mainText: {
+    fontSize: 30,
+    margin: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: "#999999",
+    margin: 10
   }
 });
